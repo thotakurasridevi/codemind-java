0e40529dd2024ef5d1006ea9b6af97306ed30813
thotakurasridevi <102217403+thotakurasridevi@users.noreply.github.com>
2022-10-08 13:33:43 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         int n,x[],i,j=0,k=0,even[],odd[];         Scanner sc =new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         even=new int[n];         odd=new int[n];
            for(i=0;i<n;i++)
            {
                if(x[i]%2==0)
                 even[j++]=x[i];
                 else
                 
                     odd[k++]=x[i];
            }
                 for(i=0;i<k;i++)
                    System.out.print(odd[i]+" ");
                 for(i=0;i<j;i++)
                 System.out.print(even[i]+" ");
                }
            }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        int n,x[],i,j=0,k=0,even[],odd[];
+        Scanner sc =new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        even=new int[n];
+        odd=new int[n];
+        
+        for(i=0;i<n;i++)
+        {
+            if(x[i]%2==0)
+             even[j++]=x[i];
+             else
+             
+                 odd[k++]=x[i];
+        }
+             for(i=0;i<k;i++)
+                System.out.print(odd[i]+" ");
+             for(i=0;i<j;i++)
+             System.out.print(even[i]+" ");
+            }
+        }
+        

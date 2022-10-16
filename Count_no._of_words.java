ab26003a8fb5a5fa969f51f06573d1cfd758ba4d
thotakurasridevi <102217403+thotakurasridevi@users.noreply.github.com>
2022-10-16 16:45:05 +0530

    import java.util.Scanner; class wordcount1 {  public static void main (String args[]) { Scanner sc=new Scanner (System.in);  String s1=sc.nextLine(); String words[]=s1.split(" ");  //System.out.println(words.length);  for(int i=0;i<words.length;i++)  {       //System.out.println(words.length);  }  System.out.println(words.length); } }

---
+++ Count_no._of_words.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class wordcount1
+{
+ public static void main (String args[])
+{
+Scanner sc=new Scanner (System.in);
+ String s1=sc.nextLine();
+String words[]=s1.split(" ");
+ //System.out.println(words.length);
+ for(int i=0;i<words.length;i++)
+ {
+      //System.out.println(words.length);
+ }
+ System.out.println(words.length);
+}
+}
